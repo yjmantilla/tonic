e077bc6f8c6728c3ed7dae0d5478d1cec8643562
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2025-03-20 17:35:10 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,19 @@
 import setuptools
 
-
 setuptools.setup(
-    name='tonic',
-    description='Tonic RL Library',
-    url='https://github.com/fabiopardo/tonic',
-    version='0.3.0',
-    author='Fabio Pardo',
-    author_email='f.pardo@imperial.ac.uk',
+    name="tonic",
+    description="Tonic RL Library",
+    url="https://github.com/fabiopardo/tonic",
+    version="0.3.0",
+    author="Fabio Pardo",
+    author_email="f.pardo@imperial.ac.uk",
+    license="MIT",
+    python_requires=">=3.6",
+    keywords=["tonic", "deep learning", "reinforcement learning"],
     install_requires=[
-        'gym', 'matplotlib', 'numpy', 'pandas', 'pyyaml', 'termcolor'],
-    license='MIT',
-    python_requires='>=3.6',
-    keywords=['tonic', 'deep learning', 'reinforcement learning'],
-    packages=[
-        "tonic",
-        "tonic.agents",
-        "tonic.environments",
-        "tonic.explorations",
-        "tonic.replays",
-        "tonic.tensorflow",
-        "tonic.torch",
-        "tonic.utils"
+        "gym", "matplotlib", "numpy", "pandas", "pyyaml", "termcolor"
     ],
+    packages=setuptools.find_packages(include=["tonic", "tonic.*"]),  # Auto-detect submodules
+    include_package_data=True,
+    zip_safe=False,  # Prevents import issues caused by zip compression
 )
